5407f9db39b8af9970bdf09e9fac0c9c20cc5b28
quylvnde <quylv.nde18063@vtc.edu.vn>
2018-11-03 16:31:32 +0700

    add new sourcưe

---
+++ demo.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct Team
+{
+    int a;
+    char b;
+};
+
+void Input();
+void Output();
+void MainMenu();
+
+int main(int argc, char const *argv[])
+{
+    MainMenu();
+
+    return 0;
+}
+
+void MainMenu()
+{
+    Input();
+    Output();
+}
+int n = 0;
+void Input()
+{
+    FILE *p;
+
+    struct Team *team;
+
+    team = (struct Team *)malloc(n * sizeof(struct Team));
+    while (1)
+    {
+        char select;
+        printf("Nhap a: ");
+        fflush(stdin);
+        scanf("%d", &team[n].a);
+
+        printf("Nhap b: ");
+        fflush(stdin);
+        scanf("%c", &team[n].b);
+
+        n++;
+
+        printf("ban co muon nhap them? (Y/N): ");
+        fflush(stdin);
+
+        scanf("%c", &select);
+
+        if (select == 'n' || select == 'N')
+        {
+            p = fopen("Data.dat", "wb");
+            fwrite(team, sizeof(struct Team), n, p);
+            fclose(p);
+            break;
+        }
+    }
+}
+
+void Output()
+{
+    FILE *file;
+    struct Team *teams;
+    file = fopen("Data.dat", "rb");
+
+    teams = (struct Team*)malloc(n * sizeof(struct Team));
+    fread(teams, sizeof(struct Team), n, file);
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("So vua nhap la: %d\n", teams[i].a);
+        printf("So vua nhap la: %c\n", teams[i].b);
+    }
+    fclose(file);
+}
